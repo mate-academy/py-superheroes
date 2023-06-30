7b74709da36d87f22e92f80c84bd88e0aadff6fe
Yaroslav Havryliv <yaroslav.h@mate.academy>
2023-06-30 16:41:23 +0300

    task created

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
-# Python Boilerplate For GitHub Tasks
+# Superheroes
 
 **Please note:** read [the guideline](https://github.com/mate-academy/py-task-guideline/blob/main/README.md)
 before starting.
+
+In this task, we will create superheroes using classes and inheritance.
+
+## Superhero Class
+The Superhero class represents a basic superhero. It has the following characteristics:
+
+### Attributes:
+- `name`: the name of the superhero;
+- `power_level`: the power level of the superhero.
+### Methods
+- `use_power()`: prints a message indicating that the superhero is using their power;
+- `calculate_power()`: calculates the power of the superhero based on their power level. Returns the calculated power multiplied by 2.
+
+Example:
+```python
+super_man = Superhero("Superman", 25)
+super_man.use_power()  # "Superman is using their power."
+print(super_man.calculate_power())  # "50"
+```
+
+## FlyingSuperhero Class
+The FlyingSuperhero class is a subclass of Superhero and represents a superhero with the ability to fly. 
+It has the following additional characteristics:
+
+### Attributes
+- `flight_speed_multiplier`: a multiplier that determines the superhero's flight speed.
+### Methods
+- `fly()`: prints a message indicating that the superhero is flying;
+- `calculate_power()`: overrides the calculate_power() method of the base class. 
+Calculates the power of the superhero based on their power level and flight speed multiplier. Returns the calculated power.
+
+Example:
+```python
+flying_superhero = FlyingSuperhero("FlyingSuperhero", 25, 3)
+flying_superhero.use_power()  # "FlyingSuperhero is using their power. "
+                              # "FlyingSuperhero is flying."
+print(flying_superhero.calculate_power())  # "125"
+flying_superhero.fly()  # "FlyingSuperhero is flying."
+```
+
+## StrengthSuperhero Class
+The StrengthSuperhero class is another subclass of Superhero and represents a superhero with exceptional strength. 
+It has the following additional characteristics:
+
+### Attributes
+- `lifting_capacity_multiplier`: a multiplier that determines the superhero's lifting capacity.
+### Methods
+- `lift_weight()`: prints a message indicating that the superhero is lifting a weight;
+- `calculate_power()`: Overrides the calculate_power() method from the base class to include the lifting capacity multiplier in the power calculation. 
+Returns the calculated power.
+
+Example:
+```python
+strength_superhero = StrengthSuperhero("StrengthSuperhero", 25, 3)
+strength_superhero.use_power()  # "StrengthSuperhero is using their power. "
+                                # "StrengthSuperhero is lifting a weight."
+print(strength_superhero.calculate_power())  # "3750"
+strength_superhero.lift_weight()  # "StrengthSuperhero is lifting a weight."
+```
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,162 @@
-# TODO: add tests
-from app.main import hello_world
+import io
+from contextlib import redirect_stdout
+from unittest import mock
+from unittest.mock import Mock
+
+import pytest
+
+import app.main as main
+from app.main import Superhero, FlyingSuperhero, StrengthSuperhero
 
 
-def test_hello_world():
-    assert hello_world() == "Hello, world!"
+@pytest.mark.parametrize(
+    "class_name", ["Superhero", "FlyingSuperhero", "StrengthSuperhero"]
+)
+def test_classes_should_be_defined(class_name: str) -> None:
+    assert hasattr(main, class_name)
+
+
+@pytest.mark.parametrize(
+    "class_name,methods",
+    [
+        ("Superhero", ["__init__", "use_power", "calculate_power"]),
+        ("FlyingSuperhero", ["__init__", "use_power", "fly", "calculate_power"]),
+        (
+            "StrengthSuperhero",
+            ["__init__", "use_power", "lift_weight", "calculate_power"],
+        ),
+    ],
+)
+def test_classes_should_have_corresponding_methods(
+    class_name: str, methods: str
+) -> None:
+    class_to_test = getattr(main, class_name)
+    for method in methods:
+        assert (
+            hasattr(class_to_test, method) is True
+        ), f"Class '{class_to_test}' should have method {method}"
+
+
+def test_superhero_use_power_method() -> None:
+    superhero = Superhero("Spider-Man", 12)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        superhero.use_power()
+    assert f.getvalue() == "Spider-Man is using their power.\n"
+
+
+def test_superhero_calculate_power_method() -> None:
+    superhero = Superhero("Spider-Man", 12)
+    assert superhero.calculate_power() == 24
+
+
+@pytest.mark.parametrize(
+    "name,power_level,flight_speed",
+    [
+        ("Superman", 20, 5),
+        ("Iron Man", 15, 3),
+    ],
+)
+def test_flying_superhero_use_power_method(
+    name: str, power_level: int, flight_speed: int
+) -> None:
+    flying_hero = FlyingSuperhero(name, power_level, flight_speed)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        flying_hero.use_power()
+    assert f.getvalue() == f"{name} is using their power.\n{name} is flying.\n"
+
+
+@pytest.mark.parametrize(
+    "name,power_level,flight_speed",
+    [
+        ("Superman", 20, 5),
+        ("Iron Man", 15, 3),
+    ],
+)
+def test_flying_superhero_fly_method(
+    name: str, power_level: int, flight_speed: int
+) -> None:
+    flying_hero = FlyingSuperhero(name, power_level, flight_speed)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        flying_hero.fly()
+    assert f.getvalue() == f"{name} is flying.\n"
+
+
+@pytest.mark.parametrize(
+    "name,power_level,flight_speed,expected_power",
+    [
+        ("Superman", 20, 5, 140),
+        ("Iron Man", 15, 3, 75),
+    ],
+)
+def test_flying_superhero_calculate_power_method(
+    name: str, power_level: int, flight_speed: int, expected_power: int
+) -> None:
+    flying_hero = FlyingSuperhero(name, power_level, flight_speed)
+    assert flying_hero.calculate_power() == expected_power
+
+
+@pytest.mark.parametrize(
+    "name,power_level,lifting_capacity",
+    [
+        ("Hulk", 25, 10),
+        ("Thor", 18, 5),
+    ],
+)
+def test_strength_superhero_use_power_method(
+    name: str, power_level: int, lifting_capacity: int
+) -> None:
+    strength_hero = StrengthSuperhero(name, power_level, lifting_capacity)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        strength_hero.use_power()
+    assert (
+        f.getvalue() == f"{name} is using their power.\n{name} is lifting a weight.\n"
+    )
+
+
+@pytest.mark.parametrize(
+    "name,power_level,lifting_capacity",
+    [
+        ("Hulk", 25, 10),
+        ("Thor", 18, 5),
+    ],
+)
+def test_strength_superhero_lift_weight_method(
+    name: str, power_level: int, lifting_capacity: int
+) -> None:
+    strength_hero = StrengthSuperhero(name, power_level, lifting_capacity)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        strength_hero.lift_weight()
+    assert f.getvalue() == f"{name} is lifting a weight.\n"
+
+
+@pytest.mark.parametrize(
+    "name,power_level,lifting_capacity,expected_power",
+    [
+        ("Hulk", 10, 10, 2000),
+        ("Thor", 18, 5, 3240),
+    ],
+)
+def test_strength_superhero_calculate_power_method(
+    name: str, power_level: int, lifting_capacity: int, expected_power: int
+) -> None:
+    strength_hero = StrengthSuperhero(name, power_level, lifting_capacity)
+    assert strength_hero.calculate_power() == expected_power
+
+
+@mock.patch.object(Superhero, "use_power")
+def test_flying_superhero_use_power_calls_super(mock_super_use_power: Mock) -> None:
+    flying_hero = FlyingSuperhero("Superman", 20, 5)
+    flying_hero.use_power()
+    mock_super_use_power.assert_called_once(), "Superhero.use_power was not called"
+
+
+@mock.patch.object(Superhero, "use_power")
+def test_strength_superhero_use_power_calls_super(mock_super_use_power: Mock) -> None:
+    strength_hero = StrengthSuperhero("Hulk", 25, 10)
+    strength_hero.use_power()
+    mock_super_use_power.assert_called_once(), "Superhero.use_power was not called"
